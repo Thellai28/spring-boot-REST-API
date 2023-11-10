abe6ac70bc7287a83e1f9cf642f0b77f2d9d186d
Thellai <thellaidhinesh96.2@gmail.com>
2023-11-10 15:35:39 +0530

    filtering ( static & dynamic ) & HAL explorer ( check in pom.xm

--- java/com/thellaidev/rest/webservices/restfulwebservices/filtering/DummyBean.java
+++ java/com/thellaidev/rest/webservices/restfulwebservices/filtering/DummyBean.java
@@ -1,26 +1,29 @@
 package com.thellaidev.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
-
-@JsonIgnoreProperties({"fied_3", "fied_2"}) 
+//@JsonIgnoreProperties({"fied_3", "fied_2"}) --> staic filtering : 
 // you can use this as an alternative to @JsonIgnore. 
 // The main difference between them is @JsonIgnore is field level property & @JsonIgnoreProperties 
 // is a class level property.
+
+@JsonFilter( "someBeanFilters" ) // Dynamic filtering : 
 public class DummyBean {
-	@JsonIgnore
+	//@JsonIgnore
 	private String fied_1;
 	
 	
 	private String fied_2;
 	private String fied_3;
-	public DummyBean(String fied_1, String fied_2, String fied_3) {
+	private String fied_4;
+	public DummyBean(String fied_1, String fied_2, String fied_3, String fied_4) {
 		super();
 		this.fied_1 = fied_1;
 		this.fied_2 = fied_2;
 		this.fied_3 = fied_3;
+		this.fied_4 = fied_4;
 	}
+	
 	public String getFied_1() {
 		return fied_1;
 	}
@@ -39,8 +42,11 @@ public class DummyBean {
 	public void setFied_3(String fied_3) {
 		this.fied_3 = fied_3;
 	}
+	public String getFied_4() {
+		return fied_4;
+	}
+	public void setFied_4(String fied_4) {
+		this.fied_4 = fied_4;
+	}
 	
-	
-	
-
 }
--- java/com/thellaidev/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ java/com/thellaidev/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -3,21 +3,40 @@ package com.thellaidev.rest.webservices.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 	
 	@GetMapping( path = "/filtering" )
-	public DummyBean filtering() {
-		return new DummyBean( "value1", "Value_2", "Value_3" );
+	public MappingJacksonValue filtering() {
+		DummyBean dummyBean = new DummyBean("verkadalai","Ellurundaii", "chicken_biriyani", " beef_fry");
+		
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue( dummyBean );
+		SimpleBeanPropertyFilter fieldsShouldRemain = SimpleBeanPropertyFilter.filterOutAllExcept("fied_2", "fied_3");
+		FilterProvider filter = new SimpleFilterProvider().addFilter("someBeanFilters", fieldsShouldRemain);
+		mappingJacksonValue.setFilters(filter);
+
+		return mappingJacksonValue;
 	}
 	
 	@GetMapping( path = "/filtering-list" )
-	public List<DummyBean> filteringList() {
-		return Arrays.asList( new DummyBean( "value1", "Value_2", "Value_3" ),
-				new DummyBean( "value1", "Value_2", "Value_3" ));
+	public MappingJacksonValue filteringList() {
+		List<DummyBean> beanList = Arrays.asList( new DummyBean( "SoyaBean", "Carrot", "chicken", "mutton" ),
+				new DummyBean( "parotta", "Dosa", "idly", "PaaniPuri" ));
+		
+		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(beanList);
+		SimpleBeanPropertyFilter fieldsShouldRemain = SimpleBeanPropertyFilter.filterOutAllExcept("fied_1", "fied_4");
+		FilterProvider filter = new SimpleFilterProvider().addFilter("someBeanFilters", fieldsShouldRemain);
+		mappingJacksonValue.setFilters(filter);
+
+		return mappingJacksonValue;
 	}
 
 }
